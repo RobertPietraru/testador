740e8811d5677caf3db622e3b94628b30115b584
RobertPietraru <rob_piet@yahoo.com>
2023-04-27 16:09:48 +0300

    image change

--- lib/features/testing/presentation/screens/home_screen.dart
+++ lib/features/testing/presentation/screens/home_screen.dart
@@ -70,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen>
                       },
                       onSelect: () {},
                       imageUrl:
-                          'https://img.freepik.com/premium-vector/job-exam-test-vector-illustration_138676-243.jpg',
+                          'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80',
                       label:
                           'Evaluare Nationala la Limba si Literatura Romana');
                 },
