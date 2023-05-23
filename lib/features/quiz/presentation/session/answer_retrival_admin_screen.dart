ca22a4c49bd3314c6c2ecfead7b752df72f72005
RobertPietraru <rob_piet@yahoo.com>
2023-05-23 20:24:45 +0300

    timer

--- lib/features/quiz/presentation/session/answer_retrival_admin_screen.dart
+++ lib/features/quiz/presentation/session/answer_retrival_admin_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:testador/features/quiz/presentation/session/timer/question_timer_cubit.dart';
 import 'package:testador/features/quiz/presentation/session/widgets/session_option_widget.dart';
 
 import '../../../../core/components/buttons/app_bar_button.dart';
@@ -29,63 +31,112 @@ class _AnswerRetrivalAdminScreenState extends State<AnswerRetrivalAdminScreen> {
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
     final state = widget.state;
-    return Scaffold(
-      appBar: CustomAppBar(
-          title: Text(splitStringIntoBlocks(widget.state.session.id),
-              style: theme.titleTextStyle),
-          trailing: [
-            AppBarButton(text: 'Continua', onPressed: widget.onContinue)
-          ]),
-      body: NestedScrollView(
-        controller: controller,
-        headerSliverBuilder: (context, _) {
-          return [
-            SliverList(
-                delegate: SliverChildListDelegate([
-              Padding(
-                  padding: theme.standardPadding.copyWith(top: 0, bottom: 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "#${(state.currentQuestionIndex + 1).toString()} ${state.currentQuestion.text ?? "Cineva a uitat sa puna aici o intrebare 😁"}",
-                        style: theme.subtitleTextStyle,
-                      ),
-                      SizedBox(height: theme.spacing.medium),
-                      if (widget.state.currentQuestion.type ==
-                          QuestionType.answer)
-                        SizedBox(height: theme.spacing.medium),
-                      if (widget.state.currentQuestion.image != null)
-                        Center(
-                          child: Container(
-                            height: 220,
-                            color: theme.secondaryColor,
-                            child: AspectRatio(
-                              aspectRatio: 1.0,
-                              child: Image.network(
-                                  widget.state.currentQuestion.image!,
-                                  fit: BoxFit.contain),
+    return BlocProvider(
+      create: (context) => QuestionTimerCubit(time: 40),
+      child: Builder(builder: (context) {
+        return BlocListener<QuestionTimerCubit, QuestionTimerState>(
+          listenWhen: (previous, current) => current.time == 0,
+          listener: (context, state) => widget.onContinue(),
+          child: Scaffold(
+            appBar: CustomAppBar(
+                title: Text(splitStringIntoBlocks(widget.state.session.id),
+                    style: theme.titleTextStyle),
+                trailing: [
+                  AppBarButton(text: 'Continua', onPressed: widget.onContinue)
+                ]),
+            body: NestedScrollView(
+              controller: controller,
+              headerSliverBuilder: (context, _) {
+                return [
+                  SliverList(
+                      delegate: SliverChildListDelegate([
+                    Padding(
+                        padding:
+                            theme.standardPadding.copyWith(top: 0, bottom: 0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    "#${(state.currentQuestionIndex + 1).toString()} ${state.currentQuestion.text ?? "Cineva a uitat sa puna aici o intrebare 😁"}",
+                                    style: theme.subtitleTextStyle,
+                                  ),
+                                ),
+                                SizedBox(width: theme.spacing.small),
+                                BlocBuilder<QuestionTimerCubit,
+                                    QuestionTimerState>(
+                                  builder: (context, state) {
+                                    return TimerWidget(seconds: state.time);
+                                  },
+                                )
+                              ],
                             ),
-                          ),
-                        ),
-                      SizedBox(height: theme.spacing.medium),
-                    ],
-                  ))
-            ]))
-          ];
-        },
-        body: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2),
-            itemCount: widget.state.currentQuestion.options.length,
-            itemBuilder: (context, index) => SessionOptionWidget(
-                  index: index,
-                  option: widget.state.currentQuestion.options[index],
-                  isSelected: false,
-                  onPressed: () => widget.onAnswerPressed(
-                      widget.state.currentQuestion.options[index]),
-                )),
-      ),
+                            SizedBox(height: theme.spacing.medium),
+                            if (widget.state.currentQuestion.type ==
+                                QuestionType.answer)
+                              SizedBox(height: theme.spacing.medium),
+                            if (widget.state.currentQuestion.image != null)
+                              Center(
+                                child: Container(
+                                  height: 220,
+                                  color: theme.secondaryColor,
+                                  child: AspectRatio(
+                                    aspectRatio: 1.0,
+                                    child: Image.network(
+                                        widget.state.currentQuestion.image!,
+                                        fit: BoxFit.contain),
+                                  ),
+                                ),
+                              ),
+                            SizedBox(height: theme.spacing.medium),
+                          ],
+                        ))
+                  ]))
+                ];
+              },
+              body: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemCount: widget.state.currentQuestion.options.length,
+                  itemBuilder: (context, index) => SessionOptionWidget(
+                        index: index,
+                        option: widget.state.currentQuestion.options[index],
+                        isSelected: false,
+                        onPressed: () => widget.onAnswerPressed(
+                            widget.state.currentQuestion.options[index]),
+                      )),
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
+
+class TimerWidget extends StatelessWidget {
+  final int seconds;
+  final double size;
+  final Color color;
+
+  const TimerWidget(
+      {super.key,
+      required this.seconds,
+      this.size = 80,
+      this.color = Colors.red});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
+    return Container(
+      width: size,
+      height: size,
+      decoration:
+          BoxDecoration(shape: BoxShape.circle, color: theme.primaryColor),
+      child: Center(
+          child: Text(seconds.toString(),
+              style: theme.titleTextStyle.copyWith(color: Colors.white))),
     );
   }
 }
--- lib/features/quiz/presentation/session/timer/question_timer_state.dart
+++ lib/features/quiz/presentation/session/timer/question_timer_state.dart
@@ -1,10 +1,9 @@
 part of 'question_timer_cubit.dart';
 
-abstract class QuestionTimerState extends Equatable {
-  const QuestionTimerState();
+class QuestionTimerState extends Equatable {
+  final int time;
+  const QuestionTimerState({required this.time});
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [time];
 }
-
-class QuestionTimerInitial extends QuestionTimerState {}
